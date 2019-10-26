0e593ed10c877d11a26b6f994da275e82d7a735b
jrboey98 <jboey@kent.edu>
2019-10-25 21:59:37 -0400

    Flesh out register
    Add confirm password and user handle

--- ProjectIncrement/src/app/register/register.component.ts
+++ ProjectIncrement/src/app/register/register.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AngularFireAuth } from '@angular/fire/auth';
+import { AngularFirestore, AngularFirestoreDocument } from '@angular/fire/firestore';
 import { auth } from 'firebase/app';
 import { Router } from '@angular/router';
 
@@ -10,22 +11,47 @@ import { Router } from '@angular/router';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor(public afAuth: AngularFireAuth, private router: Router) { }
+  constructor(public afAuth: AngularFireAuth, private afs: AngularFirestore,
+              private router: Router) { }
 
   ngOnInit() {}
 
   public register = {
     email: "",
-    password: ""
+    password: "",
+    confirmPassword: "",
+    userHandle: ""
   };
 
   public registerForm(){
     console.log(this.register);
-    this.afAuth.auth.createUserWithEmailAndPassword(this.register.email, this.register.password)
-                    .catch((error) => {
-                      let errorCode = error.code;
-                      let errorMessage = error.message;
-                      console.log(errorCode + ": " + errorMessage);
-                    })
+    if (this.register.password == this.register.confirmPassword) {
+      this.afAuth.auth.createUserWithEmailAndPassword(this.register.email, this.register.password)
+                    .then(
+                      (user) => {
+                        this.registerUser(user);
+                      },
+                      (error) => {
+                        let errorCode = error.code;
+                        let errorMessage = error.message;
+                        console.log(errorCode + ": " + errorMessage);
+                      }
+                    )
+      
+    } else {
+      console.log("Passwords do not match."); //change to an on screen message
+    }
+  }
+
+  private registerUser(user: auth.UserCredential) {
+    this.afs.collection('users').doc(user.user.uid).set({
+      email: this.register.email,
+      userHandle: this.register.userHandle
+    }).then(() => {
+      console.log("User created successfully");
+      this.router.navigate(['login']);
+    }).catch((error) => {
+      console.log("User registration failed: " + error);
+    });
   }
 }
